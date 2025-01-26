ea076bd08811b4c788eeb660a8718ddffc57c7fa
Petro Ostapuk <petroostapuk@gmail.com>
2025-01-26 17:49:17 +0200

    Fixed charter page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ const config = {
           {to: '/docs/tutorial-for-beginners', label: 'Довідник початківця', position: 'left'},
           {to: '/blog', label: 'Новини', position: 'left'},
           {to: '/about', label: 'Про нас', position: 'left'},
-          {to: '/charter', label: 'Статут', position: 'left'},
+          {to: '/charter', label: 'Статут організації', position: 'left'},
           {to: '/partners', label: 'Партнерські програми', position: 'left'},
           {
             href: 'https://github.com/uaro-radio',
